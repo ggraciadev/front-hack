7f4af840dc0fa334334514e06500e44088c57514
Rashgaron <45242200+Rashgaron@users.noreply.github.com>
2022-04-30 05:37:51 +0200

    Adding map

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,10 +1,19 @@
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import MapView from 'react-native-maps';
 function HomeScreen() {
     return(
         <View>
             <Text>Welcome from the Home Screen</Text>
+            <MapView style={styles.map}/>
         </View>
     )
 }
+
+const styles  = StyleSheet.create({
+    map: {
+        width: Dimensions.get('window').width,
+        height: Dimensions.get('window').height,
+    }
+})
 
 export { HomeScreen }
